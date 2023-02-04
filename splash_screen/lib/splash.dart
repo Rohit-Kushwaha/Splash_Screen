ac1682fcb89cfc9e5c57534547e06da52c761c94
Rohit Kushwaha <islane@Rohits-MacBook-Air.local>
2023-02-04 18:44:43 +0530

    splash_screen

---
+++ splash_screen/lib/splash.dart
@@ -0,0 +1,189 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:splash_screen/home_screen.dart';
+import 'package:splash_screen/onboarding_screen.dart';
+
+class MyCustomSplashScreen extends StatefulWidget {
+  const MyCustomSplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MyCustomSplashScreen> createState() => _MyCustomSplashScreenState();
+}
+
+class _MyCustomSplashScreenState extends State<MyCustomSplashScreen>
+    with TickerProviderStateMixin {
+  double _fontSize = 2;
+  double _containerSize = 1.5;
+  double _textOpacity = 0.0;
+  double _containerOpacity = 0.0;
+
+  late AnimationController _controller;
+  late Animation<double> animation1;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 3));
+
+    animation1 = Tween<double>(begin: 40, end: 20).animate(CurvedAnimation(
+        parent: _controller, curve: Curves.fastLinearToSlowEaseIn))
+      ..addListener(() {
+        setState(() {
+          _textOpacity = 1.0;
+        });
+      });
+
+    _controller.forward();
+
+    Timer(const Duration(seconds: 2), () {
+      setState(() {
+        _fontSize = 1.06;
+      });
+    });
+
+    Timer(const Duration(seconds: 2), () {
+      setState(() {
+        _containerSize = 1.5;
+        _containerOpacity = 1;
+      });
+    });
+
+    Timer(const Duration(seconds: 4), () {
+      setState(() {
+        Navigator.pushReplacement(
+            context, PageTransition(const MySplashCondition()));
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 246, 235, 232),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              AnimatedContainer(
+                  duration: const Duration(milliseconds: 2000),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  height: height / _fontSize),
+              AnimatedOpacity(
+                duration: const Duration(milliseconds: 1000),
+                opacity: _textOpacity,
+                child: Text(
+                  'Splash Screen',
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontFamily: "Montserrat",
+                    fontWeight: FontWeight.bold,
+                    fontSize: animation1.value,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Center(
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 2000),
+              curve: Curves.fastLinearToSlowEaseIn,
+              opacity: _containerOpacity,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 2000),
+                curve: Curves.fastLinearToSlowEaseIn,
+                height: width / _containerSize,
+                width: width / _containerSize,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                // set your image
+                child: Image.asset("assets/images/proteins.jpeg"),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PageTransition extends PageRouteBuilder {
+  final Widget page;
+
+  PageTransition(this.page)
+      : super(
+          pageBuilder: (context, animation, anotherAnimation) => page,
+          transitionDuration: const Duration(milliseconds: 100),
+          transitionsBuilder: (context, animation, anotherAnimation, child) {
+            animation = CurvedAnimation(
+              curve: Curves.fastLinearToSlowEaseIn,
+              parent: animation,
+            );
+            return Align(
+              alignment: Alignment.bottomCenter,
+              child: SizeTransition(
+                sizeFactor: animation,
+                axisAlignment: 0,
+                child: page,
+              ),
+            );
+          },
+        );
+}
+
+class MySplashCondition extends StatefulWidget {
+  const MySplashCondition({Key? key}) : super(key: key);
+
+  @override
+  State<MySplashCondition> createState() => _MySplashConditionState();
+}
+
+class _MySplashConditionState extends State<MySplashCondition>
+    with TickerProviderStateMixin {
+  late final AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 100),
+    );
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // for checking user is logged in or anything else
+      // future: cacheService.getBoolValuesSF("check_user_status"),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          // if user is logged in then go to onboarding screen after that home screen or home screen
+          return const HomeScreen();
+        } else {
+          // if user is not login then go to onboarding or signup screen or login/sign in screen
+          return const OnBoardingScreen();
+        }
+      },
+    );
+  }
+}
